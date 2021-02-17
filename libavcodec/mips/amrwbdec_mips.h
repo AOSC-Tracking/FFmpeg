0dffcee2a4c0697e7778b2102e548bf7417f7c9d
Jiaxun Yang <jiaxun.yang@flygoat.com>
2021-02-17 22:27:11 +0800

    avcodec/mips: Add switch for MADD instruction
    MADD.{d,s} is only available after MIPS IV and being dropped
    in MIPS R6 (Replaced by FMADD).
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- libavcodec/mips/amrwbdec_mips.h
+++ libavcodec/mips/amrwbdec_mips.h
@@ -54,7 +54,7 @@
 #include "config.h"
 
 #if HAVE_MIPSFPU && HAVE_INLINE_ASM
-#if !HAVE_MIPS32R6 && !HAVE_MIPS64R6
+#if HAVE_MIPSMADD
 void ff_hb_fir_filter_mips(float *out, const float fir_coef[],
                           float mem[], const float *in);
 #define hb_fir_filter ff_hb_fir_filter_mips
